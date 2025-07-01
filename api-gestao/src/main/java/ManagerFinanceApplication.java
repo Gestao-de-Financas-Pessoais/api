7f3b7ca040f3432570c81da48465bb9602aacbcd
Samarah Silva <samarah.silva@jazztech.corp>
2025-07-01 19:43:59 -0300

    User: service (incompleto), DTO, mapper, repository

---
+++ api-gestao/src/main/java/ManagerFinanceApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+@SpringBootApplication
+public class ManagerFinanceApplication {
+    //                                    Método que pode lançar exceções
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
+        SpringApplication.run(ManagerFinanceApplication.class, args);
+    }
+
+}
