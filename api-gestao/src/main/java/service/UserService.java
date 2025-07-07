7ac2ff71893867f440a3cca2aeaa2a93da989794
Samarah Silva <samarah.silva@jazztech.corp>
2025-07-06 23:23:21 -0300

    Métodos na service: criar, atualizar, deletar, buscarPorEmail

--- api-gestao/src/main/java/service/UserService.java
+++ api-gestao/src/main/java/service/UserService.java
@@ -2,20 +2,26 @@ package service;
 
 import dto.request.UserRequestDTO;
 import dto.response.UserResponseDTO;
+import exception.UsuarioNaoEncontradoException;
 import mapper.UserMapper;
 import model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.UserRepository;
+
+import java.util.Optional;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public UserResponseDTO criarUser(UserRequestDTO dto) {
+    public UserService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    public UserResponseDTO criar(UserRequestDTO dto) {
 
         User usuarioModel = UserMapper.toEntity(dto);
         User salvo = userRepository.save(usuarioModel);
@@ -23,21 +29,32 @@ public class UserService {
         return UserMapper.toDto(salvo);
     }
 
-    public UserResponseDTO buscarPorEmail(UserRequestDTO dto){
+    public UserResponseDTO atualizar(Integer id, UserRequestDTO dto) {
 
-        User usuario = UserMapper.toEntity(dto);
-        String usuarioEmail = usuario.getEmail();
+        User existente = userRepository.findById(id)
+                .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário com ID " + id + " não encontrado"));
 
-        if(userRepository.existsByEmail(usuarioEmail)){
+        existente.setNome(dto.getNome());
+        existente.setEmail(dto.getEmail());
+        existente.setSenha(dto.getSenha());
 
-            User usuarioEncontrado = userRepository.
-            return UserMapper.toDto(usuarioEmail);
-        }
-
-        new RuntimeException("E-mail não encontrado"));
-
+        User salvo = userRepository.save(existente);
+        return UserMapper.toDto(salvo);
     }
 
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
+    public void deletar(Integer id) {
+
+        boolean existe = userRepository.existsById(id);
+        if (!existe) {
+            throw new UsuarioNaoEncontradoException("Usuário com id " + id + "não encontrado");
+        }
+        userRepository.deleteById(id);
+    }
+
+    public Optional<UserResponseDTO> buscarPorEmail(String email) {
+
+        return userRepository.findByEmail(email)
+                .map(UserMapper::toDto);
+    }
+
 }
