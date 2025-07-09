0c593a1949efa4680e285558eba8f054edbc763a
Samarah Silva <samarah.silva@jazztech.corp>
2025-07-08 23:26:06 -0300

    UserController

---
+++ api-gestao/src/main/java/controller/UserController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import dto.request.UserRequestDTO;
+import dto.response.UserResponseDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.UserService;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> criar(@Valid @RequestBody UserRequestDTO dto) {
+
+        UserResponseDTO usuarioCriado = userService.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioCriado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> atualizar(@PathVariable Integer id, @RequestBody @Valid UserRequestDTO dto) {
+
+        UserResponseDTO usuarioAtualizado = userService.atualizar(id, dto);
+        return ResponseEntity.ok(usuarioAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+
+        userService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<UserResponseDTO> buscarPorEmail(@RequestParam String email){
+
+        UserResponseDTO usuario = userService.buscarPorEmail(email);
+        return ResponseEntity.ok(usuario);
+    }
+}
